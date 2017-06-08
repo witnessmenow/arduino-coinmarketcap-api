f13ff0d35cbb1fa8f1ce32739d0fca0af613390b
witnessmenow <ecenuig@gmail.com>
2017-06-08 02:22:18 +0100

    expanding example to include printing of all fields.Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # arduino-coinmarketcap-api
-A wrapper around the coinmarketcap.com api for Arduino (supports ESP8266)
+A wrapper around the [CoinMarketCap.com](http://coinmarketcap.com/) API for Arduino (supports ESP8266).
 
-## NOT WORKING YET :)
+The CoinMarketCap API can be used to look information on a wide range of crypto currency coins, included Bitcoin and Ethereum (check out the full list on their site).
+
+## Usage
+
+The simplest way to see how it works is the check out [the example that comes with the library](https://github.com/witnessmenow/arduino-coinmarketcap-api/tree/master/examples/ESP8266/GetTickerInfo).
+
+### GetTickerInfo:
+A wrapper around the single ticker endpoint supporting the convert parameter (currency)
+
+Code is as follows:
+```
+CMCTickerResponse response = api.GetTickerInfo(ticker, "eur");
+```
+
+Ticker unfortunately is not the symbol for some reason. Go to [CoinMarketCap.com](http://coinmarketcap.com/) and select the coin you would like to check, the ticker name makes up the last part of the URL. e.g: http://coinmarketcap.com/currencies/bitcoin/ , "bitcoin" is the ticker value
+
+Currency is optional, so you can pass only ticker if you want.
+Check out the currency drop down on [CoinMarketCap.com](http://coinmarketcap.com/) to get available values
+
+The return object has the following values:
+```
+struct CMCTickerResponse{
+  String id;
+  String name;
+  String symbol;
+  int rank;
+  float price_usd;
+  float price_btc;
+  float volume_usd_24h;
+  float market_cap_usd;
+  float available_supply;
+  float total_supply;
+
+  float percent_change_1h;
+  float percent_change_24h;
+  float percent_change_7d;
+  float last_updated;
+
+  float price_currency;
+  float volume_currency_24h;
+  float market_cap_currency;
+
+  String error;
+};
+```
--- examples/ESP8266/GetTickerInfo/GetTickerInfo.ino
+++ examples/ESP8266/GetTickerInfo/GetTickerInfo.ino
@@ -18,6 +18,7 @@ WiFiClientSecure client;
 CoinMarketCapApi api(client);
 
 // CoinMarketCap's limit is "no more than 10 per minute"
+// Make sure to factor in if you are requesting more than one coin.
 unsigned long api_mtbs = 60000; //mean time between api requests
 unsigned long api_due_time = 0;
 
@@ -47,45 +48,67 @@ void setup() {
 }
 
 void printTickerData(String ticker) {
+  Serial.println("---------------------------------");
   Serial.println("Getting ticker data for " + ticker);
+
+
+  // Ticker unfortunately is not the symbol for some reason.
+  // Go to CoinMarketCap.com and select the coin you would like to check
+  // The ticker name makes up the last part of the URL
+  // e.g: http://coinmarketcap.com/currencies/bitcoin/ , "bitcoin" is the ticker value
+
+  // Currency is optional, so you can pass only ticker if you want.
+  // Check out the currency drop down on CoinMarketCap.com to get available values
   CMCTickerResponse response = api.GetTickerInfo(ticker, "eur");
   if (response.error == "") {
-    Serial.println("Response:");
-
+    Serial.print("ID: ");
+    Serial.println(response.id);
+    Serial.print("Name: ");
+    Serial.println(response.name);
     Serial.print("Symbol: ");
     Serial.println(response.symbol);
+
+    Serial.print("Rank: ");
+    Serial.println(response.rank);
+
     Serial.print("Price in USD: ");
     Serial.println(response.price_usd);
+    Serial.print("Price in BTC: ");
+    Serial.println(response.price_btc);
+
+    Serial.print("24h Volume USD: ");
+    Serial.println(response.volume_usd_24h);
+    Serial.print("Market Cap USD: ");
+    Serial.println(response.market_cap_usd);
+
+    Serial.print("Available Supply: ");
+    Serial.println(response.available_supply);
+    Serial.print("Total Supply: ");
+    Serial.println(response.total_supply);
+
+    Serial.print("Percent Change 1h: ");
+    Serial.println(response.percent_change_1h);
+    Serial.print("Percent Change 24h: ");
+    Serial.println(response.percent_change_24h);
+    Serial.print("Percent Change 7d: ");
+    Serial.println(response.percent_change_7d);
+    Serial.print("Last Updated: ");
+    Serial.println(response.last_updated);
+
+    // These fields will not come back if you do not request a currency
+    Serial.print("Price in requested currecy: ");
+    Serial.println(response.price_currency);
+    Serial.print("24h Volume in requested currency: ");
+    Serial.println(response.percent_change_7d);
+    Serial.print("Market Cap in requested currency: ");
+    Serial.println(response.last_updated);
+
 
   } else {
     Serial.print("Error getting data: ");
     Serial.println(response.error);
   }
-
-  // the following fields are available in response
-  // struct CMCTickerResponse{
-  //   String id;
-  //   String name;
-  //   String symbol;
-  //   int rank;
-  //   float price_usd;
-  //   float price_btc;
-  //   float volume_usd_24h;
-  //   float market_cap_usd;
-  //   float available_supply;
-  //   float total_supply;
-  //
-  //   float percent_change_1h;
-  //   float percent_change_24h;
-  //   float percent_change_7d;
-  //   float last_updated;
-  //
-  //   float price_currency;
-  //   float volume_currency_24h;
-  //   float market_cap_currency;
-  //
-  //   String error;
-  // };
+  Serial.println("---------------------------------");
 }
 
 void loop() {
@@ -93,6 +116,7 @@ void loop() {
   if ((timeNow > api_due_time))  {
     printTickerData("bitcoin");
     printTickerData("ethereum");
+    printTickerData("dogecoin");
     api_due_time = timeNow + api_mtbs;
   }
 }
